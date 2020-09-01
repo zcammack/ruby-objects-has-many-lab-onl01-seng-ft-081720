c3e0f78e44970cc0b8983ab6719bf6954c308ed2
Zackary Cammack <zack.cammack@gmail.com>
2020-09-01 16:40:50 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -14,8 +14,8 @@ class Author
     post.author = self
   end
 
-  def add_post_by_title(title)
-    post = Post.new(title)
+  def add_post_by_title(title_post)
+    post = Post.new(title_post)
     add_post(post)
   end
 
